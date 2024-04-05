df00be04b212ab007263fe6f8f57600853becb44
Tao Xiang <46348901+leoxiang66@users.noreply.github.com>
2024-04-05 09:34:39 +0800

    1

--- leo-react/src/index.ts
+++ leo-react/src/index.ts
@@ -1,7 +1,8 @@
 import Column from "./Column";
 import Container from "./Container";
+import Floating from "./Floating";
 import Row from "./Row";
 import Sizedbox from "./Sizedbox";
 
 
-export {Column,Row,Sizedbox, Container}
+export {Column,Row,Sizedbox, Container,Floating}
